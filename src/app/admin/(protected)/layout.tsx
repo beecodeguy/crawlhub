fc1148d276fdf17f3d4fb0b167556df0faeec9aa
rujal408 <rujal408@gmail.com>
2024-08-11 08:48:07 +0545

    add sidebar

--- src/app/admin/(protected)/layout.tsx
+++ src/app/admin/(protected)/layout.tsx
@@ -1,7 +1,13 @@
+import Sidebar from "@/components/sidebar";
 import React from "react";
 
 const AdminProtectedLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="flex">
+      <Sidebar />
+      {children}
+    </div>
+  );
 };
 
 export default AdminProtectedLayout;
