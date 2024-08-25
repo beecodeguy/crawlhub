bca8b2300589fad9041b3355c8048129cd7bd6fd
Rujal Sapkota <rujal408@gmail.com>
2024-08-25 11:37:54 +0545

    working on homepage and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,13 @@ export default function Home() {
         <h1>Body</h1>
       </section>
       <footer className="h-[340px] bg-primary text-white px-[80px] py-[48px]">
-        Footer
+        <div className="flex gap-5 items-center justify-between">
+          <h4 className="typography-h4">Footer</h4>
+          <div>Solutions</div>
+          <div>Platform</div>
+          <div>Industries</div>
+          <div>Platform</div>
+        </div>
       </footer>
     </main>
   );
