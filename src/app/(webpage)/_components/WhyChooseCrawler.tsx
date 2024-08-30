24981cca03d79f1c5e18f312ca114ae68cc05f33
rujal408 <rujal408@gmail.com>
2024-08-30 12:27:29 +0545

    seperate content fr why choose crawlerhub

--- src/app/(webpage)/_components/WhyChooseCrawler.tsx
+++ src/app/(webpage)/_components/WhyChooseCrawler.tsx
@@ -6,60 +6,56 @@ import {
 } from "@/assets/icons";
 import Card from "@/components/Card";
 import IconWrapper from "@/components/IconWrapper";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import React from "react";
+
+interface IProps {
+  icon: string | StaticImport;
+  title: string;
+  description: string;
+}
+
+const CrawlerContent: React.FC<IProps> = ({ icon, title, description }) => (
+  <Card>
+    <IconWrapper icon={icon} />
+    <div className="mt-[18px] flex flex-col gap-3">
+      <span className="leading-[30px] text-[20px] font-semibold">{title}</span>
+      <p className="typography-h5">{description}</p>
+    </div>
+  </Card>
+);
 
 const WhyChooseCrawler = () => {
   return (
     <div className="flex mb-[128px] flex-col gap-[40px] items-center bg-light-grey py-5">
       <span className="typography-h2">Why Choose CrawlerHub?</span>
       <div className="flex gap-3">
-        <Card>
-          <IconWrapper icon={TailoredPrecision} />
-          <div className="mt-[18px] flex flex-col gap-3">
-            <span className="leading-[30px] text-[20px] font-semibold">
-              Tailored Precision{" "}
-            </span>
-            <p className="typography-h5">
-              Customized data extraction, delivering exactly what you need—no
-              more, no less.
-            </p>
-          </div>
-        </Card>
-        <Card>
-          <IconWrapper icon={Support24} />
-          <div className="mt-[18px] flex flex-col gap-3">
-            <span className="leading-[30px] text-[20px] font-semibold">
-              24/7 Support{" "}
-            </span>
-            <p className="typography-h5">
-              Around-the-clock services and support to ensure seamless data
-              extraction anytime.
-            </p>
-          </div>
-        </Card>
-        <Card>
-          <IconWrapper icon={UserCentric} />
-          <div className="mt-[18px] flex flex-col gap-3">
-            <span className="leading-[30px] text-[20px] font-semibold">
-              User-Centric Experience{" "}
-            </span>
-            <p className="typography-h5">
-              Hassle-free process, so you can focus on results, not the
-              technicalities.
-            </p>
-          </div>
-        </Card>
-        <Card>
-          <IconWrapper icon={TransparentPricing} />
-          <div className="mt-[18px] flex flex-col gap-3">
-            <span className="leading-[30px] text-[20px] font-semibold">
-              Transparent Pricing{" "}
-            </span>
-            <p className="typography-h5">
-              No hidden fees—just straightforward, competitive pricing.
-            </p>
-          </div>
-        </Card>
+        <CrawlerContent
+          icon={TailoredPrecision}
+          title="Tailored Precision"
+          description="Customized data extraction, delivering exactly what you need—no
+          more, no less."
+        />
+
+        <CrawlerContent
+          icon={Support24}
+          title="24/7 Support"
+          description="Around-the-clock services and support to ensure seamless data
+              extraction anytime."
+        />
+
+        <CrawlerContent
+          icon={UserCentric}
+          title="User-Centric Experience"
+          description="Hassle-free process, so you can focus on results, not the
+              technicalities."
+        />
+
+        <CrawlerContent
+          icon={TransparentPricing}
+          title="Transparent Pricing"
+          description="No hidden fees—just straightforward, competitive pricing."
+        />
       </div>
     </div>
   );
