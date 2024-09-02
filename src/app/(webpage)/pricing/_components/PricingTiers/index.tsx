9239b98ab9c7615c9e3bc7e07e0aafd735d1c664
rujal408 <rujal408@gmail.com>
2024-09-02 10:58:36 +0545

    working on pricing page

--- src/app/(webpage)/pricing/_components/PricingTiers/index.tsx
+++ src/app/(webpage)/pricing/_components/PricingTiers/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const PricingTiers = () => {
-  return <div>PricingTiers</div>;
+  return (
+    <div className="container mx-auto flex flex-col gap-6 items-center mb-[128px]">
+      <div className="flex flex-col gap-2 w-[654px] items-center">
+        <span className="typography-h2">Pricing Tiers</span>
+        <p className="typography-h5 text-center">
+          While each project is unique, we offer three general pricing tiers to
+          give you an idea of what to expect. Final pricing will be determined
+          after our assessment.
+        </p>
+      </div>
+      <div>Table</div>
+    </div>
+  );
 };
 
 export default PricingTiers;
