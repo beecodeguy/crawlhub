4acd75e7b77bc465c60172db3918208491932523
Rujal Sapkota <rujal408@gmail.com>
2024-09-04 14:21:55 +0545

    created separate component for blog image with title or label

--- src/app/(webpage)/blog/_component/BlogCard.tsx
+++ src/app/(webpage)/blog/_component/BlogCard.tsx
@@ -1,8 +1,28 @@
 import { ClockIcon } from "@/assets/icons";
 import { DefaultAvatar, SampleImage } from "@/assets/images";
 import JumpButton from "@/components/JumpButton";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import React from "react";
+
+interface TImageLabel {
+  icon: string | StaticImport;
+  height: number;
+  width: number;
+  label: string;
+}
+
+const ImageWithLabel: React.FC<TImageLabel> = ({
+  icon,
+  height,
+  width,
+  label,
+}) => (
+  <div className="flex gap-1 items-center">
+    <Image src={icon} alt="" height={height} width={width} />
+    <span>{label}</span>
+  </div>
+);
 
 const BlogCard = () => {
   return (
@@ -24,14 +44,19 @@ const BlogCard = () => {
           Walmart via Google Ratings & Reviews Data.
         </p>
         <div className="flex gap-2 typography-h5">
-          <div className="flex gap-1 items-center">
-            <Image src={DefaultAvatar} alt="" height={32} width={32} />
-            <span>Sova</span>
-          </div>
-          <div className="flex gap-1 items-center">
-            <Image src={ClockIcon} alt="" height={24} width={24} />
-            <span>November 26, 2023</span>
-          </div>
+          <ImageWithLabel
+            label="Sova"
+            icon={DefaultAvatar}
+            height={32}
+            width={32}
+          />
+
+          <ImageWithLabel
+            label="November 26, 2023"
+            icon={ClockIcon}
+            height={24}
+            width={24}
+          />
         </div>
         <JumpButton label="Read More" className="mt-4" />
       </div>
