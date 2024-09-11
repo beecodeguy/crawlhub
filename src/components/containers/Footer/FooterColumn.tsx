117730ad84741eabe718139c2d2d43115cfbec4c
rujal408 <rujal408@gmail.com>
2024-09-11 11:29:43 +0545

    made footer responsive

--- src/components/containers/Footer/FooterColumn.tsx
+++ src/components/containers/Footer/FooterColumn.tsx
@@ -1,3 +1,9 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import Link from "next/link";
 import React from "react";
 
@@ -10,18 +16,40 @@ interface IProps {
   }[];
 }
 
+type IList = Pick<IProps, "linkList">;
+
+const FooterItemList: React.FC<IList> = ({ linkList }) => (
+  <div className="flex flex-col mt-3 gap-2">
+    {linkList.map((list) => (
+      <Link href={list.link} key={list.id} className="text-sm typography-f1">
+        {list.name}
+      </Link>
+    ))}
+  </div>
+);
+
 const FooterColumn: React.FC<IProps> = ({ name, linkList }) => {
+  
   return (
-    <div>
-      <div className="uppercase typography-h5">{name}</div>
-      <div className="flex flex-col mt-3 gap-2">
-        {linkList.map((list) => (
-          <Link href={list.link} key={list.id} className="text-sm">
-            {list.name}
-          </Link>
-        ))}
+    <>
+      <Accordion type="single" collapsible className="md:hidden xs:block">
+        <AccordionItem
+          className="border-none border-b-1 border-white"
+          value={name}
+        >
+          <AccordionTrigger className="typography-h5 p-0 font-normal uppercase">
+            {name}
+          </AccordionTrigger>
+          <AccordionContent className="p-0">
+            <FooterItemList linkList={linkList} />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <div className="md:block xs:hidden">
+        <div className="uppercase typography-h5">{name}</div>
+        <FooterItemList linkList={linkList} />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/containers/Footer/index.tsx
+++ src/components/containers/Footer/index.tsx
@@ -7,54 +7,58 @@ const Footer = () => {
   return (
     <footer className="bg-primary mt-[128px]">
       <div className="container mx-auto flex flex-col gap-2 py-6 text-white">
-        <div className="flex gap-4 justify-between">
+        <div className="flex gap-4 md:flex-row xs:flex-col">
           <span className="typography-h4">CrawlerHub</span>
-          <FooterColumn
-            name="Solutions"
-            linkList={[
-              { id: 1, name: "Web Scraping", link: "" },
-              { id: 2, name: "Web Scraping API", link: "" },
-              { id: 3, name: "Data Abstraction", link: "" },
-              { id: 4, name: "Consulting Service", link: "" },
-            ]}
-          />
+          <div className="flex md:flex-row xs:flex-col md:ga-0 xs:gap-3 justify-between w-full">
+            <FooterColumn
+              name="Solutions"
+              linkList={[
+                { id: 1, name: "Web Scraping", link: "" },
+                { id: 2, name: "Web Scraping API", link: "" },
+                { id: 3, name: "Data Abstraction", link: "" },
+                { id: 4, name: "Consulting Service", link: "" },
+              ]}
+            />
 
-          <FooterColumn
-            name="Industries"
-            linkList={[
-              { id: 1, name: "E-Commerce", link: "" },
-              { id: 2, name: "Housing and Real Estate", link: "" },
-              { id: 3, name: "Retail and Trading", link: "" },
-              { id: 4, name: "Management and Consulting", link: "" },
-              { id: 5, name: "Analytics", link: "" },
-            ]}
-          />
+            <FooterColumn
+              name="Industries"
+              linkList={[
+                { id: 1, name: "E-Commerce", link: "" },
+                { id: 2, name: "Housing and Real Estate", link: "" },
+                { id: 3, name: "Retail and Trading", link: "" },
+                { id: 4, name: "Management and Consulting", link: "" },
+                { id: 5, name: "Analytics", link: "" },
+              ]}
+            />
 
-          <FooterColumn
-            name="Support"
-            linkList={[
-              { id: 1, name: "Help Center", link: "" },
-              { id: 2, name: "FAQs", link: "" },
-              { id: 3, name: "Blog", link: "" },
-            ]}
-          />
+            <FooterColumn
+              name="Support"
+              linkList={[
+                { id: 1, name: "Help Center", link: "" },
+                { id: 2, name: "FAQs", link: "" },
+                { id: 3, name: "Blog", link: "" },
+              ]}
+            />
 
-          <FooterColumn
-            name="Company"
-            linkList={[
-              { id: 1, name: "About Us", link: "" },
-              { id: 2, name: "Careers", link: "" },
-              { id: 3, name: "Pricing", link: "" },
-            ]}
-          />
+            <FooterColumn
+              name="Company"
+              linkList={[
+                { id: 1, name: "About Us", link: "" },
+                { id: 2, name: "Careers", link: "" },
+                { id: 3, name: "Pricing", link: "" },
+              ]}
+            />
+          </div>
         </div>
         <div className="h-[1px] bg-grey-light" />
-        <div className="flex justify-between typography-b1">
-          <div className="flex gap-2">
+        <div className="flex md:flex-row xs:flex-col-reverse xs:gap-2 justify-between typography-b1">
+          <div className="flex gap-2 typography-f2">
             <span className="underline">Terms of Service</span>
             <span className="underline">Privacy Policy</span>
           </div>
-          <div>&copy; 2024 CrawlerHub. All rights reserved.</div>
+          <div className="typography-f1">
+            &copy; 2024 CrawlerHub. All rights reserved.
+          </div>
           <div className="flex gap-4">
             <Image src={FaceBookIcon} height={10} width={20} alt="Facebook" />
             <Image src={LinkedInIcon} height={10} width={20} alt="LinkedIn" />
