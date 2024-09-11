4fe7631ae1194256f348b89604ad9c1c2ddc245b
Rujal Sapkota <rujal408@gmail.com>
2024-09-11 08:37:02 +0545

    implement chain middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { chain } from "./middlewares/chain";
+import { withAuthMiddleware } from "./middlewares/withRouteMiddleware";
+import { withApiMiddleware } from "./middlewares/withApiMiddleware";
 
-const secret = process.env.NEXTAUTH_SECRET;
-
-export async function middleware(request: NextRequest) {
-  const session = await getToken({ req: request, secret });
-  const role = session?.role;
-  const pathname = request?.nextUrl?.pathname;
-  if (pathname.includes("/admin/users")) {
-    if (role !== "admin") {
-      return NextResponse.redirect(new URL("/admin/dashboard", request.url));
-    }
-  }
-
-  return NextResponse.next();
-}
+export default chain([withAuthMiddleware, withApiMiddleware]);
 
 export const config = {
   matcher: ["/admin/:path*"],
